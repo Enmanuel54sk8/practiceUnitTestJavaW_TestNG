79b2d6eaadc6e87bd54a60898b893f65188a2e27
Emmanuel Valenzuela <32858351+Softx0@users.noreply.github.com>
2019-02-14 08:02:02 -0800

    test done of BeforeMethod similar to Before on JUnit

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,20 +1,41 @@
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class CalculadoraTest {
 
-    Calculadora calculadora;
+    private Calculadora calculadora;
+    private int numeroUno;
+    private int numeroDos;
+
+    @BeforeClass
+    public void instanceClass(){
+        System.out.println("instance Class");
+        calculadora = new Calculadora();
+    }
 
     @BeforeMethod
-    public void instanceClass(){
-        calculadora = new Calculadora();
+    public void setUp(){
+        System.out.println("setUp all");
+        numeroUno = 5;
+        numeroDos = 10;
     }
 
    @Test
     public void testAdd(){
+       System.out.println("testAdd");
        int expected = 27;
        assertEquals(expected, calculadora.add(20,7));
    }
+
+   @Test
+   public void testAddRandomNumbers(){
+       System.out.println("testAddRandomNumbers");
+        assertTrue(numeroUno+5 == numeroDos);
+   }
+
+
 }
